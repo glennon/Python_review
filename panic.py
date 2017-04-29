5e65db2561f24f05ca39cf6cf856dd36e3065ef5
glennon <glennon@gmail.com>
2017-04-29 00:35:24 -0700

    page 67 exercise

--- panic.py
+++ panic.py
@@ -3,6 +3,13 @@ plist = list(phrase)
 print(phrase)
 print(plist)
 
+plist.remove('D')
+plist.insert(4, 'a')
+plist = plist[:7]
+plist.remove(' ')
+plist.remove("'")
+plist.insert(2, ' ')
+
 new_phrase = ''.join(plist)
 print(plist)
 print(new_phrase)
