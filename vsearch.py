ac20d48a676bf8fcd7e70ec794ccd8d13b9db8dc
glennon <glennon@gmail.com>
2017-05-04 01:10:21 -0700

    return experiments

--- vsearch.py
+++ vsearch.py
@@ -1,5 +1,4 @@
 def search4vowels(word):
   """Display any vowels found in an input word """
   vowels = set('aeiou')
-  found = sorted(vowels.intersection(set(word)))
-  return bool(found)
+  return vowels.intersection(set(word))
