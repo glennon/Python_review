5577029509397c3b1e8e1c63edee2e43eae6b1e3
glennon <glennon@gmail.com>
2017-05-04 00:59:18 -0700

    pass arguments to function

--- vsearch.py
+++ vsearch.py
@@ -1,7 +1,6 @@
-def search4vowels():
+def search4vowels(word):
   """Display any vowels found in an input word """
   vowels = set('aeiou')
-  word = input('Provide a word to search for vowels: ')
   found = sorted(vowels.intersection(set(word)))
   for vowel in found:
     print(vowel)
