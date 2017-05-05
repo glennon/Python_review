1273622c9fc7eb84bfd9bb9e1b264d974337575d
glennon <glennon@gmail.com>
2017-05-04 17:39:12 -0700

    generalization of previous vowel search function

--- vsearch.py
+++ vsearch.py
@@ -2,3 +2,7 @@ def search4vowels(phrase:str) -> set:
   """Display any vowels found in an input word """
   vowels = set('aeiou')
   return vowels.intersection(set(phrase))
+
+def search4letters(phrase:str, letters:str) -> set:
+  """Display any 'letters' found in an input word """
+  return set(letters).intersection(set(phrase))
