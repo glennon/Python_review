9ca895939261c525b1fc74793ccc7574f311cdcb
glennon <glennon@gmail.com>
2017-05-04 17:40:27 -0700

    added default values to function arguments

--- vsearch.py
+++ vsearch.py
@@ -3,6 +3,6 @@ def search4vowels(phrase:str) -> set:
   vowels = set('aeiou')
   return vowels.intersection(set(phrase))
 
-def search4letters(phrase:str, letters:str) -> set:
+def search4letters(phrase:str='the quick brown fox jumped over the lazy dog', letters:str='aeiou') -> set:
   """Display any 'letters' found in an input word """
   return set(letters).intersection(set(phrase))
