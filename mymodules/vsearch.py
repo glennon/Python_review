0a04ff3afd3cf49106da5a991717452a63982918
glennon <glennon@gmail.com>
2017-05-06 17:43:03 -0700

    pep8 compatibility

--- mymodules/vsearch.py
+++ mymodules/vsearch.py
@@ -1,8 +1,9 @@
-def search4vowels(phrase:str) -> set:
-  """Display any vowels found in an input word """
-  vowels = set('aeiou')
-  return vowels.intersection(set(phrase))
+def search4vowels(phrase: str) -> set:
+    """Display any vowels found in an input word """
+    vowels = set('aeiou')
+    return vowels.intersection(set(phrase))
 
-def search4letters(phrase:str='the quick brown fox jumped over the lazy dog.', letters:str='aeiou') -> set:
-  """Display any 'letters' found in an input word """
-  return set(letters).intersection(set(phrase))
+
+def search4letters(phrase: str='the fox jumped.', letters: str='aeiou') -> set:
+    """Display any 'letters' found in an input word """
+    return set(letters).intersection(set(phrase))
