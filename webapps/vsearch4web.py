b68931faf7bc1fe0030b288402511216f702e467
glennon <glennon@gmail.com>
2017-05-15 01:58:03 -0700

    multiple decorators instead of redirect

--- webapps/vsearch4web.py
+++ webapps/vsearch4web.py
@@ -3,10 +3,6 @@ from vsearch import search4letters
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello() -> str:
-  return 'Hello world from Flask!'
-
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
     phrase = request.form['phrase']
@@ -15,6 +11,7 @@ def do_search() -> 'html':
     results = str(search4letters(phrase,letters))
     return render_template('results.html', the_phrase=phrase,the_letters=letters,the_title=title,the_results=results)
 
+@app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', the_title='Welcome to search3letters on the web!')
