2a81e956ed7e51cfaf397d57a806f9f99a25ff26
glennon <glennon@gmail.com>
2017-05-15 01:24:25 -0700

    add post method to app.route

--- webapps/vsearch4web.py
+++ webapps/vsearch4web.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello() -> str:
   return 'Hello world from Flask!'
 
-@app.route('/search4')
+@app.route('/search4', methods=['POST'])
 def do_search() -> str:
     return str(search4letters('life, the universe, and everything','eiru,!'))
 
