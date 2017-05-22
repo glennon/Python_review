31aae7a0af74d67dea531425c45c1ca4eb28a76e
glennon <glennon@gmail.com>
2017-05-22 15:04:55 -0700

    add dunder repr representation

--- countfromby.py
+++ countfromby.py
@@ -4,6 +4,11 @@ class CountFromBy:
         self.val = v
         self.incr = i
 
+
+    def __repr__(self) -> str:
+        return str(self.val)
+    
+
     def increase(self) -> None:
         self.val += self.incr
 
