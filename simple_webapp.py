c8c6ad3226aa74292e23158c5c3bcc9967b0274f
glennon <glennon@gmail.com>
2017-05-24 16:47:35 -0700

    added logout and status

--- simple_webapp.py
+++ simple_webapp.py
@@ -24,6 +24,18 @@ def do_login() -> str:
     session['logged_in'] = True
     return 'You are now logged in'
 
+@app.route('/logout')
+def do_logout() -> str:
+    session.pop('logged_in')
+    return 'You are now logged out'
+
+@app.route('/status')
+def check_status() -> str:
+    if 'logged_in' in session:
+        return 'You are currently logged in.'
+    return 'You are not logged in.'
+
+
 app.secret_key = 'YouWillNeverGuessMySecretKey'
 
 
