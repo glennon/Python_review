084cc2ade3df114db3f51df2751d10bddc92bfed
glennon <glennon@gmail.com>
2017-05-24 16:11:03 -0700

    sessions and state

---
+++ quick_session.py
@@ -0,0 +1,11 @@
+from flask import Flask, session
+
+app = Flask(__name__)
+
+app.secret_key = 'YouWillNeverGuess'
+
+
+@app.route('/setuser/<user>')
+def setuser(user: str) -> str:
+    session['user'] = user
+    return 'User value set to: ' + session['user']
