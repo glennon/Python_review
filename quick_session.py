8e3fdcf952982c4bdd521f63fda5f372d043612e
glennon <glennon@gmail.com>
2017-05-24 16:18:49 -0700

    retrieve session variable

--- quick_session.py
+++ quick_session.py
@@ -9,3 +9,11 @@ app.secret_key = 'YouWillNeverGuess'
 def setuser(user: str) -> str:
     session['user'] = user
     return 'User value set to: ' + session['user']
+
+
+@app.route('/getuser')
+def getuser() => str:
+    return 'User value is currently set to: ' + session['user']
+
+if __name__ == '__main__':
+    app.run(debug=True)
