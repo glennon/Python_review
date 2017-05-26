dddf742c5856817b584d35a9538229f16be9f0a6
glennon <glennon@gmail.com>
2017-05-26 16:27:23 -0700

    arbitrary number of keyword arguments

--- myfunc.py
+++ myfunc.py
@@ -5,3 +5,10 @@ def myfunc(*args):
         print()
     else:
         print('no inputs')
+
+def myfunc2(**kwargs):
+    for k,v in kwargs.items():
+        print(k,v, sep='->', end=' ')
+    if kwargs:
+        print()
+
